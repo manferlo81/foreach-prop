9dfb3e491a0c12b07fff70d0bbf61bda5045a049
Manuel Fernandez <manferlo81@gmail.com>
2019-04-30 18:59:37 +0700

    fixed types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,13 @@
 
-declare type EachPropCallback<K extends keyof any, V, E extends any[]> = (value: V, key: K, ...extra: E) => any;
+declare type EachPropCallback<K extends keyof any, V, E extends any[]> = (value: V, key: K, ...extra: E) => void;
+declare type MapCallback<K extends keyof any, V, E extends any[], N = any> = (value: V, key: K, ...extra: E) => N;
+declare type FilterCallback<K extends keyof any, V, E extends any[]> = (value: V, key: K, ...extra: E) => any;
 declare type ReduceCallback<K extends keyof any, V, E extends any[], T = any> = (prev: T, value: V, key: K, ...extra: E) => T;
 
 declare function forEach<K extends keyof any, V, E extends any[]>(object: Record<K, V>, callback: EachPropCallback<K, V, E>, ...extra: E): void;
-declare function map<K extends keyof any, V, E extends any[], N = any>(object: Record<K, V>, callback: EachPropCallback<K, V, E>, ...extra: E): Record<K, N>;
+declare function map<K extends keyof any, V, E extends any[], N = any>(object: Record<K, V>, callback: MapCallback<K, V, E, N>, ...extra: E): Record<K, N>;
 declare function keyOf<K extends keyof any, V>(object: Record<K, V>, value: V): K | null;
-declare function findKey<K extends keyof any, V, E extends any[]>(object: Record<K, V>, callback: EachPropCallback<K, V, E>, ...extra: E): K | null;
+declare function findKey<K extends keyof any, V, E extends any[]>(object: Record<K, V>, callback: FilterCallback<K, V, E>, ...extra: E): K | null;
 declare function reduce<K extends keyof any, V, E extends any[], T = any>(object: Record<K, V>, callback: ReduceCallback<K, V, E, T>, initial: T, ...extra: E): T;
 
 export { forEach, map, keyOf, findKey, reduce };
