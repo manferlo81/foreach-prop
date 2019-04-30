e6d77225414618c31a6565ddf29076d944f70f20
Manuel Fernandez <manferlo81@gmail.com>
2019-05-01 00:19:46 +0700

    rename map src file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export { default as forEach } from "./for-each";
-export { default as map } from "./obj-map";
+export { default as map } from "./map";
 export { default as keyOf } from "./key-of";
 export { default as lastKeyOf } from "./last-key-of";
 export { default as findKey } from "./find-key";
