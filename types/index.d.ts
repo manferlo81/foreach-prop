4899e25c7381826d8da3a35023948a6b69951ffd
Manuel Fernandez <manferlo81@gmail.com>
2019-04-30 17:14:10 +0700

    refactored types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,7 @@
+
 declare type EachPropCallback<K extends keyof any, V> = (value: V, key: K) => any;
-declare function eachProp<K extends keyof any, V>(object: Record<K, V>, callback: EachPropCallback<K, V>): K | null;
-export = eachProp;
+declare function forEach<K extends keyof any, V>(object: Record<K, V>, callback: EachPropCallback<K, V>): void;
+declare function map<K extends keyof any, V, N = any>(object: Record<K, V>, callback: EachPropCallback<K, V>): Record<K, N>;
+declare function findKey<K extends keyof any, V>(object: Record<K, V>, callback: EachPropCallback<K, V>): K | null;
+
+export { forEach, map, findKey };
