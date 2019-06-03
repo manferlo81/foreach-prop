db5377ff3dd286c93340fa623791a132b07b3fa8
Manuel Fernandez <manferlo81@gmail.com>
2019-06-03 12:00:13 +0700

    findKey refactoring

--- src/find-key.ts
+++ src/find-key.ts
@@ -1,5 +1,4 @@
 import toArray from "args-to-arr";
-import callItBack from "./call-it-back";
 import hasOwn from "./has-own";
 import { FilterCallback } from "./types";
 
@@ -15,15 +14,44 @@ function findKey<K extends keyof any, V, E extends any[], TH = any>(
   callback: FilterCallback<K, V, E, TH>,
 ): K | null {
 
-  const extra = toArray(arguments, 2);
+  const args = arguments;
+  const extraLen = args.length - 2;
 
-  for (const key in object) {
-    if (
-      hasOwn.call(object, key) &&
-      callItBack(callback, this, object, key, extra as E)
-    ) {
-      return key;
+  if (extraLen === 0) {
+
+    for (const key in object) {
+      if (
+        hasOwn.call(object, key) &&
+        callback.call<TH, any, any>(this, object[key], key)
+      ) {
+        return key;
+      }
     }
+
+  } else if (extraLen === 1) {
+
+    for (const key in object) {
+      if (
+        hasOwn.call(object, key) &&
+        callback.call<TH, any, any>(this, object[key], key, args[2])
+      ) {
+        return key;
+      }
+    }
+
+  } else {
+
+    const extra = toArray(args, 2) as E;
+
+    for (const key in object) {
+      if (
+        hasOwn.call(object, key) &&
+        callback.call<TH, any, any>(this, object[key], key, ...extra)
+      ) {
+        return key;
+      }
+    }
+
   }
 
   return null;
