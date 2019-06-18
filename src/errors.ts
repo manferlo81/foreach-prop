1b3dd8975f5dbd886ee3f53f52999834336fa3cf
Manuel Fernandez <manferlo81@gmail.com>
2019-06-18 15:30:50 +0700

    error functions return type

--- src/errors.ts
+++ src/errors.ts
@@ -1,15 +1,15 @@
-export function error(msg: string) {
+export function error(msg: string): TypeError {
   return new TypeError(msg);
 }
 
-export function notEnoughArgs(count: number, expected: number) {
+export function notEnoughArgs(count: number, expected: number): TypeError {
   return error(`expected ${expected} arguments, got ${count}.`);
 }
 
-export function invalidObject(object: unknown) {
+export function invalidObject(object: unknown): TypeError {
   return error(`${object} is not an object.`);
 }
 
-export function invalidCallback(callback: unknown) {
+export function invalidCallback(callback: unknown): TypeError {
   return error(`${callback} is not a function.`);
 }
