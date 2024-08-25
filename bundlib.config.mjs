3396663c31f8b2a0586a087a48b779918b275e52
Manuel Fernández <manferlo81@gmail.com>
2024-08-25 18:41:46 +0700

    chore(deps): Update bundlib and use ESM for config

---
+++ bundlib.config.mjs
@@ -0,0 +1,7 @@
+import { config } from 'bundlib';
+
+export default config({
+  name: 'eachProp',
+  min: 'browser',
+  project: 'tsconfig-build.json',
+});
--- bundlib.config.ts
+++
@@ -1,9 +0,0 @@
-import { BundlibConfig } from 'bundlib';
-
-const config: BundlibConfig = {
-  name: 'eachProp',
-  min: 'browser',
-  project: 'tsconfig-build.json',
-};
-
-export default config;
