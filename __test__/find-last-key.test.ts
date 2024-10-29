adcd70eb2379bcdf22c00c3266074df563c7eb5d
Manolin <manferlo81@gmail.com>
2024-10-29 12:59:27 +0700

    feat: Add `findLastKey` method (#1126)
    * feat: Implement `findLastKey` method
    * chore: Document `findLastKey` method
    * chore: Add `findLastKey` to keywords

---
+++ __test__/find-last-key.test.ts
@@ -0,0 +1,147 @@
+import { findLastKey } from '../src';
+import { createObjectWithProto } from './tools/create-object';
+import { normalizeObject } from './tools/helpers';
+import { reverseArray } from './tools/reverse-array';
+import { UnknownFunction } from './tools/types';
+import { invalidCallbacks, invalidObjects } from './tools/values';
+
+describe('findLastKey method', () => {
+
+  test('should throw on insufficient arguments', () => {
+    const __findLastKey = findLastKey as UnknownFunction;
+    const cases = [
+      () => __findLastKey(),
+      () => __findLastKey({}),
+    ];
+    cases.forEach((exec) => {
+      expect(exec).toThrow(TypeError);
+    });
+  });
+
+  test('should throw on non object', () => {
+    invalidObjects.forEach((object) => {
+      const exec = () => findLastKey(object as never, () => null);
+      expect(exec).toThrow(TypeError);
+    });
+  });
+
+  test('should throw on invalid predicate function', () => {
+    invalidCallbacks.forEach((predicate) => {
+      const exec = () => findLastKey({}, predicate as never);
+      expect(exec).toThrow(TypeError);
+    });
+  });
+
+  test('should skip prototype properties', () => {
+
+    const callback = jest.fn();
+
+    const ownProps = ['ownPropA', 'ownPropB'] as const;
+    const protoProps = ['protoPropA', 'protoPropB'] as const;
+    const object = createObjectWithProto(protoProps, ownProps);
+
+    findLastKey(object, callback);
+
+    expect(callback).toHaveBeenCalledTimes(ownProps.length);
+    reverseArray(ownProps).forEach((key, index) => {
+      expect(callback).toHaveBeenNthCalledWith(index + 1, key, key);
+    });
+    protoProps.forEach((key) => {
+      expect(callback).not.toHaveBeenCalledWith(key, key);
+    });
+
+  });
+
+  test('should pass key and value to predicate function', () => {
+
+    const [object, entries] = normalizeObject({ a: 1, b: 2, c: 3, d: 2 });
+
+    const callback = jest.fn(() => false);
+
+    findLastKey(object, callback);
+
+    expect(callback).toHaveBeenCalledTimes(entries.length);
+    reverseArray(entries).forEach(([key, value], index) => {
+      expect(callback).toHaveBeenNthCalledWith(
+        index + 1,
+        value,
+        key,
+      );
+    });
+
+  });
+
+  test('should pass this argument to callback', () => {
+
+    const [object, entries] = normalizeObject({ a: 1, b: 2 });
+
+    const thisArg = {};
+    const callback = jest.fn(function (this: unknown) {
+      expect(this).toBe(thisArg);
+      return false;
+    });
+
+    findLastKey.call(thisArg, object, callback);
+
+    expect(callback).toHaveBeenCalledTimes(entries.length);
+
+  });
+
+  test('should pass extra arguments to predicate function', () => {
+
+    const [object, entries] = normalizeObject({ a: 1, b: 2 });
+
+    const callback = jest.fn();
+
+    const extra1 = {};
+    const extra2: never[] = [];
+
+    findLastKey(object, callback, extra1, extra2);
+
+    expect(callback).toHaveBeenCalledTimes(entries.length);
+    expect(callback).toHaveBeenCalledWith(
+      expect.anything(),
+      expect.any(String),
+      extra1,
+      extra2,
+    );
+
+  });
+
+  test('should return the found key', () => {
+    const object = { a: 1, b: 2, c: 3, d: 4 };
+    const cases = [
+      { value: 1, expectedKey: 'a' },
+      { value: 2, expectedKey: 'b' },
+      { value: 3, expectedKey: 'c' },
+      { value: 4, expectedKey: 'd' },
+    ];
+    cases.forEach(({ value, expectedKey }) => {
+      const result = findLastKey(object, (val) => {
+        return val === value;
+      });
+      expect(result).toBe(expectedKey);
+    });
+  });
+
+  test('should return first key that matches', () => {
+    // IMPORTANT!
+    // Because object key don't follow a specific order,
+    // any key can be considered as the "first key that matches"
+
+    const object = { a: 1, b: 1, c: 1, d: 1 };
+    const key = findLastKey(object, (value) => value === 1);
+
+    expect(key).not.toBeNull();
+    expect(Object.keys(object)).toContain(key);
+  });
+
+  test('should return null if not found', () => {
+    const object = { a: 1, b: 2, c: 3, d: 2, e: '' };
+    const result = findLastKey(object, (val) => {
+      return val === 'does-not-exist';
+    });
+    expect(result).toBeNull();
+  });
+
+});
---
+++ src/methods/find-last-key.ts
@@ -0,0 +1,47 @@
+import { ensureIsObject, ensureMinLength } from '../tools/ensure';
+import { findEntryKeyByPredicate } from '../tools/find-entry';
+import { getEntries } from '../tools/object-entries';
+import type { PredicateCallbackFromObject } from '../types/callback-types';
+import type { EntryKeyTypeFromObject } from '../types/entry-types';
+import type { UnknownArray } from '../types/helper-types';
+
+export function findLastKey<O extends object, T = unknown>(
+  this: T,
+  object: O,
+  predicate: PredicateCallbackFromObject<O, [], T>,
+): EntryKeyTypeFromObject<O> | null;
+
+export function findLastKey<O extends object, X extends UnknownArray, T = unknown>(
+  this: T,
+  object: O,
+  predicate: PredicateCallbackFromObject<O, X, T>,
+  ...extra: X
+): EntryKeyTypeFromObject<O> | null;
+
+export function findLastKey<O extends object, T = unknown>(
+  this: T,
+  object: O,
+  predicate: PredicateCallbackFromObject<O, UnknownArray, T>,
+  ...extra: UnknownArray
+): EntryKeyTypeFromObject<O> | null;
+
+export function findLastKey<O extends object, X extends UnknownArray, T = unknown>(
+  this: T,
+  object: O,
+  predicate: PredicateCallbackFromObject<O, X, T>,
+  ...extra: X
+): EntryKeyTypeFromObject<O> | null {
+
+  // throw if not enough arguments
+  ensureMinLength(arguments.length, 2);
+
+  // throw if not an object
+  ensureIsObject(object);
+
+  // get entries
+  const entries = getEntries(object).reverse();
+
+  // find key
+  return findEntryKeyByPredicate(this, entries, predicate, extra);
+
+}
