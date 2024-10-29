e01c66844d8992c7e70f71abee528b373fddfe80
Manuel Fernández <manferlo81@gmail.com>
2024-10-29 22:56:21 +0700

    chore: More descriptive type

--- src/tools/find-entry.ts
+++ src/tools/find-entry.ts
@@ -1,4 +1,4 @@
-import type { MapCallbackFromEntry } from '../types/callback-types';
+import type { PredicateCallbackFromEntry } from '../types/callback-types';
 import type { EntryPredicate } from '../types/entry-callback-types';
 import type { EntryKeyType, EntryValueType, UnknownEntry } from '../types/entry-types';
 import type { UnknownArray } from '../types/helper-types';
@@ -22,7 +22,7 @@ export function findEntryKeyByValue<E extends UnknownEntry>(entries: E[], value:
   );
 }
 
-export function findEntryKeyByPredicate<E extends UnknownEntry, X extends UnknownArray, T>(thisArg: T, entries: E[], predicate: MapCallbackFromEntry<E, unknown, X, T>, extra: X): EntryKeyType<E> | null {
+export function findEntryKeyByPredicate<E extends UnknownEntry, X extends UnknownArray, T>(thisArg: T, entries: E[], predicate: PredicateCallbackFromEntry<E, X, T>, extra: X): EntryKeyType<E> | null {
   return findEntryKey(
     entries,
     createMapEntryCallback(
