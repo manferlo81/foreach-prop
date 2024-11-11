5e31ad552f05604175c7849b5c4f40ec2c3ef8b5
Manuel Fernández <manferlo81@gmail.com>
2024-11-11 14:43:16 +0700

    chore: Refactor workflows

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,12 +3,12 @@ const config = {
   cacheDirectory: 'node_modules/.cache/jest',
   preset: 'ts-jest',
 
-  collectCoverage: !process.env.SKIP_COVERAGE,
+  collectCoverage: process.env.COVERAGE !== 'SKIP',
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.ts',
   ],
-  coverageReporters: process.env.CI
+  coverageReporters: process.env.COVERAGE === 'CI'
     ? ['json', 'clover', 'cobertura']
     : ['html', 'text'],
 
