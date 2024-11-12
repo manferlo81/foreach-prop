14e2ed1cd3c16cb4cfd4447392bcc0b77708cb5d
Manuel Fernández <manferlo81@gmail.com>
2024-11-12 11:45:16 +0700

    chore: Cleanup workflows

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,14 +1,18 @@
+const { COVERAGE: COVERAGE_ENV } = process.env;
+const collectCoverage = COVERAGE_ENV !== 'SKIP';
+const coverageOnCI = COVERAGE_ENV === 'CI';
+
 /** @type { import("jest").Config } */
 const config = {
   cacheDirectory: 'node_modules/.cache/jest',
   preset: 'ts-jest',
 
-  collectCoverage: process.env.COVERAGE !== 'SKIP',
+  collectCoverage,
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.ts',
   ],
-  coverageReporters: process.env.COVERAGE === 'CI' || process.env.CI
+  coverageReporters: coverageOnCI
     ? ['json', 'clover', 'cobertura']
     : ['html', 'text'],
 
